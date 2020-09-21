bac988f0e241db56419658c6aa5a70f1df5137e1
sandeeprachha <rachhasandeepkumar.4a3>
2020-09-21 13:40:11 +0530

    Initial commit for pods

---
+++ Example/SamplePod/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SamplePod
+//
+//  Created by Sandeep Rachha on 09/21/2020.
+//  Copyright (c) 2020 Sandeep Rachha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
